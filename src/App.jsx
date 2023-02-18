05ac866697eb1cf71c5ec370c10a8e0a33315651
Muhamad Usama Zuberi <usama.zuberi1010@gmail.om>
2023-02-18 18:49:11 +0500

    Setup: Project Structure & Theme

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.scss";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="main">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <section className="section">
+        Hello World
+      </section>
+    </div>
+  );
+};
+
+export default Home;
